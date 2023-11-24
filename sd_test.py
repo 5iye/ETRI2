bd8b5ea843c8cf1afc946816cc82c6b3917dc874
5iye <112414191+5iye@users.noreply.github.com>
2023-11-24 10:59:46 +0900

    Update sd_test.py

--- sd_test.py
+++ sd_test.py
@@ -79,7 +79,7 @@ for t in tqdm(scheduler.timesteps):
     with torch.no_grad():
         noise_pred = unet(latent_model_input, 
                           t, 
-                          encoder_hidden_states=text_embeddings)
+                          encoder_hidden_states=text_embeddings).sample
 
     # perform guidance
     noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
