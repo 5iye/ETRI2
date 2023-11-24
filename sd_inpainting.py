4bdee3aabc81fae8c614683ecc0838682e3361fa
5iye <112414191+5iye@users.noreply.github.com>
2023-11-24 16:32:56 +0900

    Update sd_inpainting.py

--- sd_inpainting.py
+++ sd_inpainting.py
@@ -6,8 +6,8 @@ pipeline = AutoPipelineForInpainting.from_pretrained(
     "runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16
 ).to("cuda")
 
-pipeline.enable_model_cpu_offload()
-pipeline.enable_xformers_memory_efficient_attention()
+# pipeline.enable_model_cpu_offload()
+# pipeline.enable_xformers_memory_efficient_attention()
 
 
 init_image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/inpaint.png")
@@ -16,4 +16,5 @@ mask_image = load_image("https://huggingface.co/datasets/huggingface/documentati
 generator = torch.Generator("cuda").manual_seed(92)
 prompt = "concept art digital painting of an elven castle, inspired by lord of the rings, highly detailed, 8k"
 image = pipeline(prompt=prompt, image=init_image, mask_image=mask_image, generator=generator).images[0]
-make_image_grid([init_image, mask_image, image], rows=1, cols=3)
+image= make_image_grid([init_image, mask_image, image], rows=1, cols=3)
+image.save('./result/test_image.jpg')
